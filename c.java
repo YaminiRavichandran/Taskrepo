e38a5330d05611be2bf274a0e5c1c9fd5856f7ff
yaminir@gmail.com <myEmail@eg.com>
2023-03-17 13:34:07 +0530

    c.java is added

---
+++ c.java
@@ -0,0 +1,12 @@
+package packMarch;
+
+public class test1 {
+    public static void main(String[] args) {
+        int b = 99;
+        System.out.println(++b);
+        System.out.println(b);
+        System.out.println(b++);
+        System.out.println(b);
+    }
+
+}
